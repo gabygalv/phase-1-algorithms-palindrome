b369698049cd8cd741b9fe928c73070f5889e7b1
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-02-02 18:13:44 -0700

    completed palindrome lab

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+    // const wordToArray = word.split();
+    // const reverseWord = wordToArray.reverse().join();
+    // wordToArray === reverseWord ? (isPalindrome(true)) : (isPalindrome(false));
+    //not sure why this didnt work
 
+    const length = word.length;
+    for (let i = 0; i < length / 2; i++) {
+      if (word[i]!== word[length - 1 - i]) {
+        return false;
+    
+    }
+    return true;
+}}
 /* 
   Add your pseudocode here
+  strings need to be converted to lowercase
+  check if the first and last characters are the same
+  for the length of the string
+
+  how to check if the first and last characters are the same?
+  and then repeat the process for the rest of the string
 */
 
 /*
